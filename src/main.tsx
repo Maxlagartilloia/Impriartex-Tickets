5a540fb7059a7d2028357c780828298ad86a787e
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-02-01 00:33:20 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Aquí es donde se cargan los estilos 3D que creamos
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
